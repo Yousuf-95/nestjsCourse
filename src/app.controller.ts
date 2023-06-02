7897807c3ee01aeaae8000e0d6f768cb48472991
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-06-02 20:58:17 +0530

    defining routes in controller decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Get } from "@nestjs/common";
 
+// Any route applied to 'Controller' decorator will apply to 
+// all the routes defined in the controller.
+// Routes in this controller are: 'nest/' and 'nest/bye'
 
-@Controller()
+@Controller('nest')
 export class AppController {
-    @Get()
+    @Get('/')
     getRootRoute() {
         return 'Hello from NestJS';
+    }
+
+    @Get('/bye')
+    getByeRoute() {
+        return 'Bye';
     }
 }
