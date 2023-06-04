088946f68a6933ec8866c16c65a8ff7a77be3b6d
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-06-04 17:35:43 +0530

    cleanup for section 3

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { NestFactory } from '@nestjs/core';
-
-@Module({
-    controllers: [AppController]
-})
-export class AppModule {}
