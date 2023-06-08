60bf6f28bfe45890528b28b9555494f132d7dc96
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-06-08 22:15:58 +0530

    create message repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,17 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+    async findOne(id: string) {
+
+    }
+
+    async findAll() {
+
+    }
+
+    async create(message: string) {
+
+    }
+
+
+}
