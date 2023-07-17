caf0a1dbfd652e6b5e988a0283e8a2ea8a89831d
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-07-17 22:16:54 +0530

    feat: create user and store in sqlite

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+
 @Controller('auth')
 export class UsersController {
-    @Post('/signup')
+    private usersService: UsersService;
 
+    constructor(usersService: UsersService) {
+        this.usersService = usersService;
+    }
+
+    @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         //Create user logic
-        console.log(body);
+        this.usersService.create(body.email, body.password);
     }
 }
