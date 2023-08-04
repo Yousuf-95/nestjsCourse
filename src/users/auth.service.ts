2f9506beaa05741976783e8584aac32fa126e227
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-08-04 22:49:52 +0530

    complete signup endpoint

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,28 +1,39 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt} from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
-    constructor (private usersService: UsersService) {}
+    constructor(private usersService: UsersService) { }
 
 
     async signup(email: string, password: string) {
         // see if email already exists
         const users = await this.usersService.find(email);
-        if(users.length) {
+        if (users.length) {
             throw new BadRequestException('email already in use');
         }
 
         // Hash user password
+        // Generate a salt
+        const salt = randomBytes(8).toString('hex');
 
+        // Hash the salt and the password together
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        // Join the hashed result and the salt together
+        const result = salt + '.' + hash.toString('hex');
 
         // create new user and save
-
+        const user = await this.usersService.create(email, result);
 
         // return the user
+        return user;
     }
 
-    signin() {
+    // signin() {
 
-    }
+    // }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,19 +4,22 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
     private usersService: UsersService;
+    private authService: AuthService;
 
-    constructor(usersService: UsersService) {
+    constructor(usersService: UsersService, authService: AuthService) {
         this.usersService = usersService;
+        this.authService = authService;
     }
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
+        return this.authService.signup(body.email, body.password);
     }
 
     @Get('/:id')
