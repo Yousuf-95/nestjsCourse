c23c63dc237222c831525156cb16fb0a414d566f
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-08-29 19:37:13 +0530

    add a new e2e test case

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -23,10 +23,28 @@ describe('Authentication System', () => {
       .send({ email, password: 'abcdefg' })
       .expect(201)
       .then((res) => {
-        const {id, email} = res.body;
+        const { id, email } = res.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       })
+  });
+
+  it('/auth/signup (POST) -> /auth/whoami (GET)', async () => {
+    const email = 'abc@email.com';
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'abcdefg' })
+      .expect(201)
+
+    const cookie = response.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email);
   });
 
 });
