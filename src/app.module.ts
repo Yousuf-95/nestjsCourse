faf9fbaa652c5ce92e816a9e8dd079fb81c43d1e
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-09-06 21:41:06 +0530

    set cookieSession key using env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,10 +41,12 @@ const cookieSession = require('cookie-session');
   providers: [AppService, { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) }],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(
       cookieSession({
-        keys: ['cookieSession']
+        keys: [this.configService.get('COOKIE_KEY')]
       })
     ).forRoutes('*');
   }
