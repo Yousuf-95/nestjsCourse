2323a4ac150aa44452057b82acd4c580b8f393ff
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-09-09 10:49:37 +0530

    add migration file & update typeorm config for prod

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,12 +7,34 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
     constructor(private configService: ConfigService) { }
 
     createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-        return {
-            type: 'sqlite',
-            synchronize: process.env.NODE_ENV === 'test' ? true : false,
-            database: this.configService.get<string>('DB_NAME'),
-            autoLoadEntities: true,
-            migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-        };
+        if (process.env.NODE_ENV === 'test') {
+            return {
+                type: 'sqlite',
+                synchronize: true,
+                database: this.configService.get<string>('DB_NAME'),
+                autoLoadEntities: true,
+                migrationsRun: true,
+            };
+        }
+        else if (process.env.NODE_ENV === 'development') {
+            return {
+                type: 'sqlite',
+                synchronize: false,
+                database: this.configService.get<string>('DB_NAME'),
+                autoLoadEntities: true,
+                migrationsRun: false,
+            }
+        }
+        else if (process.env.NODE_ENV === 'production') {
+            return {
+                type: 'postgres',
+                url: process.env.DATABASE_RL,
+                migrationsRun: true,
+                entities: ['**/*.entity.js'],
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            }
+        }
     }
 }
